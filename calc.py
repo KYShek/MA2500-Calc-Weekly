402c174e8be02c0b9e10bda8d4d7ab013549a3c2
Josh Wu <rainyfeeling@outlook.com>
2022-05-13 22:18:48 +0800

    refactor(coding style): trivial: add spaces around =
    Signed-off-by: Josh Wu <rainyfeeling@outlook.com>

--- calc.py
+++ calc.py
@@ -38,11 +38,11 @@ def get_stock_code_name(stock_code):
     return result.loc[0,'code_name']
 
 def calc_MA2500(stock_code):
-    t=dt.datetime.utcnow()
-    t=t+dt.timedelta(hours=8)               # 当前的北京时间
-    t11=t+dt.timedelta(days=-365*11-3)      # 得到11年前的时间，减去3个闰年
-    d=t.strftime('%Y-%m-%d')
-    d11=t11.strftime('%Y-%m-%d')
+    t = dt.datetime.utcnow()
+    t = t + dt.timedelta(hours=8)               # 当前的北京时间
+    t11 = t + dt.timedelta(days=-365*11-3)      # 得到11年前的时间，减去3个闰年
+    d = t.strftime('%Y-%m-%d')
+    d11 = t11.strftime('%Y-%m-%d')
     #TIME
     result = pd.DataFrame()
 
@@ -50,23 +50,23 @@ def calc_MA2500(stock_code):
     # 得到三栏的表格: date日期, code证卷代码, close收盘的点数。
     k = bs.query_history_k_data_plus(stock_code,"date,code,close",start_date=d11, end_date=d,frequency="d", adjustflag="3")
     # 得到k线数据
-    result=pd.concat([result,k.get_data()],axis=0,ignore_index=True)
-    result.date=pd.to_datetime(result.date)
-    result=result.sort_values(by='date',ascending=False)        # 按日期排序，今天日期放在最上面
-    result=result.reset_index(drop=True)
+    result = pd.concat([result,k.get_data()],axis=0,ignore_index=True)
+    result.date = pd.to_datetime(result.date)
+    result = result.sort_values(by='date',ascending=False)        # 按日期排序，今天日期放在最上面
+    result = result.reset_index(drop=True)
 
-    today=pd.DataFrame()
+    today = pd.DataFrame()
     # 得到最近2500天的收盘点数列表
-    today['close']=result.nlargest(2500,'date').close
+    today['close'] = result.nlargest(2500,'date').close
     # 计算MA2500均值
-    MA2500=today.close.astype(float).mean()
+    MA2500 = today.close.astype(float).mean()
     # 计算MA2500的上下约20%的浮动区间, 并且只取两位小数
-    MAdiv=int(MA2500/1.2*100)/100
-    MAmul=int((MA2500*1.2)*100)/100
-    MA2500=int(MA2500*100)/100
+    MAdiv = int(MA2500/1.2*100)/100
+    MAmul = int((MA2500*1.2)*100)/100
+    MA2500 = int(MA2500*100)/100
     # 今天的收盘
-    close_today=int(float(result.loc[0,'close'])*100)/100
-    date_today=result.loc[0,'date']
+    close_today = int(float(result.loc[0,'close'])*100)/100
+    date_today = result.loc[0,'date']
     date_today_str = str(date_today.date()) + ": "
 
     '''
@@ -85,18 +85,18 @@ def calc_MA2500(stock_code):
     #     高于MA2500线: =
     #     高于x1.2线:   ✗
     #
-    if(close_today<=MAdiv):
+    if (close_today <= MAdiv):
         recommend = "✔"
         title = date_today_str + stock_name + "低于÷1.2线" + ": " + recommend
     else:
-        if(close_today<=MA2500):
-            judge="÷1.2"
+        if (close_today <= MA2500):
+            judge = "÷1.2"
             recommend = "✓"
-        elif(close_today<=MAmul):
-            judge="MA2500"
+        elif (close_today <= MAmul):
+            judge = "MA2500"
             recommend = "="
         else:
-            judge="*1.2"
+            judge = "*1.2"
             recommend = "✗"
         title = date_today_str + stock_name + "高于"+judge+"线" + ": " + recommend
 
