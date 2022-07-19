a761702579dbee82188e98c4d8047d9cba5da375
KinYuShek <52855847+KinYuShek@users.noreply.github.com>
2022-07-19 14:24:34 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -66,6 +66,45 @@ def main():
         test_out = cont.replace('\n','\n\n')
         print(cont)
         send_server(tilt,cont) #插入在需要推送的地方，我这里的"Her said"是我的标题，msg是我前面爬取的消息'''
+        
+        #TIME
+        result1 = pd.DataFrame()
+        k = bs.query_history_k_data_plus("sz.399001","date,code,close",start_date=d11, end_date=d,frequency="d", adjustflag="3")
+        result1=pd.concat([result1,k.get_data()],axis=0,ignore_index=True)
+        result1.date=pd.to_datetime(result1.date)
+        result1=result1.sort_values(by='date',ascending=False)
+        result1=result1.reset_index(drop=True)
+        #ESSENTIAL DATA
+        today=pd.DataFrame()
+        today['close']=result1.nlargest(2500,'date').close
+        MA2500=today.close.astype(float).mean()
+        MAdiv=int(MA2500/1.2*100)/100
+        MAmul=int((MA2500*1.2)*100)/100
+        MA2500=int(MA2500*100)/100
+        close_today=int(float(result1.loc[0,'close'])*100)/100
+        #CALC MA2500  
+        print(MAdiv)
+        print(MA2500)
+        print(MAmul)
+        print(close_today)
+        #
+        if(close_today<=MAdiv):
+            tilt="今日深成指低于÷1.2线"
+        else:
+            if(close_today<=MA2500):
+                judge="÷1.2"
+            elif(close_today<=MAmul):
+                judge="MA2500"
+            else:
+                judge="*1.2"
+            tilt="今日深成指高于"+judge+"线"
+        print(tilt)
+        #GENERATE TITLE
+        # 斜杠用来代码换行
+        cont="今日深成指收盘: "+str(close_today)+"\n今日MA2500数据 \n\t *1.2: "+str(MAmul)+"\n\t 均："+str(MA2500)+"\n\t /1.2: "+str(MAdiv)
+        test_out = cont.replace('\n','\n\n')
+        print(cont)
+        send_server(tilt,cont) #插入在需要推送的地方，我这里的"Her said"是我的标题，msg是我前面爬取的消息'''
         bs.logout
     except Exception:
         error = "服务异常"
